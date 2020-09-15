2b54f8c185a251469346fb2a9c772100d697d427
Bronwyn Carr <bronwynacarr@gmail.com>
2020-09-14 19:08:19 -0700

    refactored password code into methods

--- lib/app.rb
+++ lib/app.rb
@@ -9,14 +9,27 @@
 
 
 
-password = ""
 
-10.times do
-  ascii_decimal = rand(33..126)
-  password = password + ascii_decimal.chr
+def get_password_length
+  puts "How many characters would you like your password to be?"
+  print "> "
+
+  gets.chomp.to_i #implicit return of number
 end
 
-puts password
+
+def generate_password
+  password = "" # ititalise outside of loop or will be reassigned each time
+  get_password_length.times do #g et_password_length is bumber returned earlier. iterated over "times"
+    ascii_decimal = rand(33..126) # generate random asci based on number
+    password = password + ascii_decimal.chr # adds asci to password
+  end
+  password # implicit return of new password
+end
+
+p generate_password
+
+
 
 
 
